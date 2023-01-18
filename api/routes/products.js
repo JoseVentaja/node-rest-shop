42c077fe92e8099fe7155d58bf35f47d1d29d202
JoseVentaja <ventajatic@gmail.com>
2023-01-18 14:40:13 +0100

    Implement MongoDB

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const { default: mongoose } = require('mongoose');
 const router = express.Router();
+const Product = require('../models/products')
 
 router.get('/', (req, res, next) => {
     res.status(200).json({
@@ -7,27 +9,45 @@ router.get('/', (req, res, next) => {
     })
 })
 router.post('/', (req, res, next) => {
-    const product = {
+
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
         price: req.body.price
-    };
-    res.status(201).json({
-        message: 'Handling Post request to /products',
-        createdProduct: product
+
+    });
+    product.save().then(result => {
+        console.log(result);
+        res.status(201).json({
+            message: 'Handling Post request to /products',
+            createdProduct: product
+        })
     })
+        .catch(error => {
+            console.log(error)
+            res.status(500).json({
+                error: error
+            })
+        });
+        
 })
 router.get('/:productId', (req, res, next) => {
     const id = req.params.productId;
-    if (id === 'special') {
-        res.status(200).json({
-            message: 'You discovered special ID',
-            id: id
-        });
-    } else {
-        res.status(200).json({
-            message: 'You passed an ID'
-        });
-    }
+    Product.findById(id)
+        .exec()
+        .then(doc =>{
+            console.log("From database",doc)
+            if (doc){
+            res.status(200).json(doc);
+            } else {
+                res.status(404).json({message: 'No valid entry found for provided ID'});
+            }
+            res.status(200).json(doc)
+        })
+        .catch(error=> {
+            console.log(error)
+            res.status(500).json({error : error})
+        })
 });
 router.patch('/:productId', (req, res, next) => {
     res.status(200).json({
